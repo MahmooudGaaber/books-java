15ee2e90dc489d87453739d2351cc5281fab0903
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-05-29 21:52:35 +0300

    books: create GET books endpoint

---
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.luv2code.books.controller;
+
+import com.luv2code.books.entity.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class BookController {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController(){
+        initializeBooks();
+    }
+
+    private void initializeBooks(){
+        books.addAll(List.of(
+                new Book("title one ","author two","science"),
+                new Book("title two","author two","science"),
+                new Book("title three","author three","history"),
+                new Book("title four","author four","math"),
+                new Book("title five","author five","math"),
+                new Book("title six","author six","math")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+
+}
